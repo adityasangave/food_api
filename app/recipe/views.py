bf05a1a7ffa33d33a4c8c7523fb4447a7e3db368
adii21-Ux <adityasangave2112@gmail.com>
2022-02-10 23:03:01 +0530

    Completed tags app

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.mixins import ListModelMixin,CreateModelMixin
+from rest_framework import viewsets,generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Tag
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.GenericViewSet, ListModelMixin, CreateModelMixin):
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
